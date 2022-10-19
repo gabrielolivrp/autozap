daf9f6343292ea127ffd143791d5016c43b126ec
Gabriel Augusto <gabrielolivrp@gmail.com>
2022-10-19 20:19:31 -0300

    remove `MutationObserver` to get the qr code

--- src/autozap/getQrCodeInBase64.ts
+++ src/autozap/getQrCodeInBase64.ts
@@ -1,45 +1,31 @@
+import { getWhatsAppStatus } from './getWhatsAppStatus'
 import { WhatsApp } from './types'
 
-export interface QrcodeResult {
+export interface QrCodeResult {
   base64: string
   urlCode: string
 }
 
 export async function getQrCodeInBase64(
   instance: WhatsApp
-): Promise<QrcodeResult | undefined> {
-  const isAuthenticated = await instance.page.evaluate(() => {
-    return new Promise(async (resolve, _reject) => {
-      const fn = () => {
-        if (document.querySelector('.landing-wrapper') !== null) {
-          resolve(false)
-        } else if (document.querySelector('.app-wrapper-web') !== null) {
-          resolve(true)
-        }
-      }
-      const mo = new MutationObserver(fn)
-      mo.observe(document.body, { childList: true, subtree: true })
-      fn()
-    })
-  })
+): Promise<QrCodeResult | undefined> {
+  const status = await getWhatsAppStatus(instance)
 
-  if (!isAuthenticated) {
-    return instance.page.evaluate(() => {
-      return new Promise(async (resolve, _reject) => {
-        const fn = () => {
-          const qr = document.querySelector('canvas')
-          const image = qr?.closest('[data-ref]')
-          if (qr && image) {
-            resolve({
-              base64: qr.toDataURL(),
-              urlCode: image.getAttribute('data-ref')!,
-            })
+  if (status === 'UNPAIRED') {
+    return await instance.page
+      .waitForFunction(() => {
+        const qr = document.querySelector('canvas')
+        const image = qr?.closest('[data-ref]')
+        if (qr && image) {
+          return {
+            base64: qr.toDataURL(),
+            urlCode: image.getAttribute('data-ref')!,
           }
         }
-        const mo = new MutationObserver(fn)
-        mo.observe(document.body, { childList: true, subtree: true })
-        fn()
+        return false
       })
-    })
+      // @ts-ignore
+      .then(async (x) => await x.evaluate((a) => a))
+      .catch((e) => e)
   }
 }
---
+++ src/autozap/getWhatsAppStatus.ts
@@ -0,0 +1,42 @@
+import { WhatsApp } from './types'
+
+type WhatsAppStatus = 'UNPAIRED' | 'PAIRING' | 'CONNECTED'
+
+export async function getWhatsAppStatus(
+  instance: WhatsApp
+): Promise<WhatsAppStatus> {
+  return await instance.page
+    .waitForFunction(
+      () => {
+        if (
+          document.querySelector('.landing-wrapper') &&
+          document.querySelector('canvas')
+        ) {
+          return 'UNPAIRED'
+        }
+
+        const streamStatus =
+          window['Store'] &&
+          window['Store'].Stream &&
+          window['Store'].Stream.displayInfo
+
+        if (['PAIRING', 'RESUMING', 'SYNCING'].includes(streamStatus)) {
+          return 'PAIRING'
+        }
+
+        const chat: HTMLDivElement = document.querySelector('.app, .two')!
+        if (chat && chat.attributes && chat.tabIndex) {
+          return 'CONNECTED'
+        }
+
+        return false
+      },
+      {
+        timeout: 0,
+        polling: 100,
+      }
+    )
+    // @ts-ignore
+    .then(async (x) => await x.evaluate((a) => a))
+    .catch((e) => e)
+}
