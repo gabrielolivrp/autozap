e91790c8e8435c4a2aec41841acb8d4e04bfda6c
Gabriel Augusto <gabrielolivrp@gmail.com>
2022-10-19 22:58:50 -0300

    add multi-instance examples

--- examples/multi/index.ts
+++ examples/multi/index.ts
@@ -1,3 +1,32 @@
-import { createInstance } from 'autozap'
+import {
+  asciiQrCode,
+  createInstance,
+  getQrCodeInBase64,
+  sendMessage,
+  WhatsApp,
+} from 'autozap'
 
-console.log('multi')
+const makeInstance = (id: string) =>
+  createInstance({
+    id,
+    // more configs...
+  })
+
+async function main() {
+  const instances: Record<string, WhatsApp> = {
+    zap1: await makeInstance('zap1'),
+    zap2: await makeInstance('zap2'),
+  }
+
+  console.log('QRCODE 1')
+  let result = await getQrCodeInBase64(instances.zap1)
+  console.log(await asciiQrCode(result!.urlCode))
+
+  console.log('QRCODE 2')
+  let result2 = await getQrCodeInBase64(instances.zap2)
+  console.log(await asciiQrCode(result2!.urlCode))
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => console.error(err))
